2c020c80af748ace904a841aca2c31b34a24d2c6
Udoy <srudoy436@gmail.com>
2023-09-03 11:35:24 +0600

    Use Products.js file

--- Projects/amazon/scripts/amazon.js
+++ Projects/amazon/scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      starts: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      starts: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      starts: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -87,4 +57,3 @@ console.log(productsHTML);
 
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
