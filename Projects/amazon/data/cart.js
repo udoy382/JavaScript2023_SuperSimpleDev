3efef3228aae02a923e36d0801586a28687d132a
Udoy <srudoy436@gmail.com>
2023-09-12 11:47:30 +0600

    Delete products from cart when clicking delete

--- Projects/amazon/data/cart.js
+++ Projects/amazon/data/cart.js
@@ -1,10 +1,13 @@
-export const cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
@@ -23,4 +26,15 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+export function removeFormCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
--- Projects/amazon/scripts/checkout.js
+++ Projects/amazon/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFormCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -29,7 +29,7 @@ cart.forEach((cartItem) => {
           ${matchingProduct.name}
         </div>
         <div>
-          ${formatCurrency(product.priceCent)}
+        ${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -89,3 +89,11 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId
+    removeFormCart(productId);
+    console.log(cart)
+  })
+})
